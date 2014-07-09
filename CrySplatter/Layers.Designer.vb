0cfb9ac6340c13b44806355ee860154deb911238
Sasha Petrovich <sasha@allegragames.com>
2014-07-10 01:45:45 +1200

    Parsing xml

--- CrySplatter/Layers.Designer.vb
+++ CrySplatter/Layers.Designer.vb
@@ -26,16 +26,20 @@ Partial Class Layers
         Me.TextBoxFileContent = New System.Windows.Forms.TextBox()
         Me.ButtonBack = New System.Windows.Forms.Button()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBoxLayerProperties = New System.Windows.Forms.RichTextBox()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TextBoxFileContent
         '
-        Me.TextBoxFileContent.Location = New System.Drawing.Point(13, 12)
+        Me.TextBoxFileContent.Location = New System.Drawing.Point(12, 25)
         Me.TextBoxFileContent.Multiline = True
         Me.TextBoxFileContent.Name = "TextBoxFileContent"
         Me.TextBoxFileContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBoxFileContent.Size = New System.Drawing.Size(661, 238)
+        Me.TextBoxFileContent.Size = New System.Drawing.Size(660, 49)
         Me.TextBoxFileContent.TabIndex = 0
         '
         'ButtonBack
@@ -43,7 +47,7 @@ Partial Class Layers
         Me.ButtonBack.BackColor = System.Drawing.Color.Transparent
         Me.ButtonBack.DialogResult = System.Windows.Forms.DialogResult.Cancel
         Me.ButtonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.ButtonBack.Location = New System.Drawing.Point(12, 279)
+        Me.ButtonBack.Location = New System.Drawing.Point(12, 321)
         Me.ButtonBack.Name = "ButtonBack"
         Me.ButtonBack.Size = New System.Drawing.Size(102, 29)
         Me.ButtonBack.TabIndex = 7
@@ -56,17 +60,55 @@ Partial Class Layers
         Me.PictureBox2.Image = Global.CrySplatter.My.Resources.Resources.round_logo300
         Me.PictureBox2.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(684, 320)
+        Me.PictureBox2.Size = New System.Drawing.Size(684, 362)
         Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox2.TabIndex = 10
         Me.PictureBox2.TabStop = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(13, 104)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(154, 199)
+        Me.ListBox1.TabIndex = 11
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "XML Content"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Layers"
+        '
+        'RichTextBoxLayerProperties
+        '
+        Me.RichTextBoxLayerProperties.Location = New System.Drawing.Point(185, 104)
+        Me.RichTextBoxLayerProperties.Name = "RichTextBoxLayerProperties"
+        Me.RichTextBoxLayerProperties.Size = New System.Drawing.Size(487, 95)
+        Me.RichTextBoxLayerProperties.TabIndex = 14
+        Me.RichTextBoxLayerProperties.Text = ""
         '
         'Layers
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.ClientSize = New System.Drawing.Size(684, 320)
+        Me.ClientSize = New System.Drawing.Size(684, 362)
+        Me.Controls.Add(Me.RichTextBoxLayerProperties)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.ButtonBack)
         Me.Controls.Add(Me.TextBoxFileContent)
         Me.Controls.Add(Me.PictureBox2)
@@ -84,4 +126,8 @@ Partial Class Layers
     Friend WithEvents TextBoxFileContent As System.Windows.Forms.TextBox
     Friend WithEvents ButtonBack As System.Windows.Forms.Button
     Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents RichTextBoxLayerProperties As System.Windows.Forms.RichTextBox
 End Class
--- CrySplatter/Layers.vb
+++ CrySplatter/Layers.vb
@@ -1,4 +1,8 @@
-﻿Public Class Layers
+﻿Imports System.Xml
+Imports System.IO
+Imports System.Text
+
+Public Class Layers
 
     Private xmlContent As String
     Private heightmapContent As Byte()
@@ -6,11 +10,31 @@
     Private heightmapFilePos As Integer
     Private layermapFilePos As Integer
 
+    Private Structure LayerRec
+        Public Name As String
+        Public Id As Byte
+        Public SplatFilename As String
+        Public SplatLoaded As Byte()
+        Public SplatGenerated As Byte()
+    End Structure
+
+    Private Structure LayerStr
+        Public Size As Integer
+        Public Arr As LayerRec()
+    End Structure
+
+    Private LayersInfo As LayerStr
+
     Private Sub Layers_VisibleChanged(sender As Object, e As EventArgs) Handles MyBase.VisibleChanged
 
         If Me.Visible = True Then
-            LoadHeightmapDatFile()
-            ParseXml()
+            Try
+                LoadHeightmapDatFile()
+                ParseXml()
+                DisplayLayersInfo()
+            Catch ex As Exception
+                MessageBox.Show(ex.ToString())
+            End Try
         End If
     End Sub
 
@@ -33,7 +57,7 @@
 
         ' read the xml
         Dim xmlArray(xmlLength) As Byte
-        Array.Copy(fileContent, readPos, xmlArray, 0, xmlLength)
+        Array.Copy(fileContent, readPos, xmlArray, 0, xmlLength - 1)
         xmlContent = System.Text.Encoding.UTF8.GetString(xmlArray)
         TextBoxFileContent.Text = xmlContent
         readPos += xmlLength
@@ -60,6 +84,42 @@
     End Sub
 
     Private Sub ParseXml()
+        Dim LayerName As String = "", LayerId As String = ""
+
+        ReDim LayersInfo.Arr(-1)
+        LayersInfo.Size = -1
+
+        Using reader As XmlReader = XmlReader.Create(New StringReader(xmlContent))
+            Try
+                While reader.ReadToFollowing("Layer")
+
+                    While reader.MoveToNextAttribute()
+                        If reader.Name = "Name" Then
+                            LayerName = reader.Value
+                        ElseIf reader.Name = "LayerId" Then
+                            LayerId = reader.Value
+
+                            ReDim Preserve LayersInfo.Arr(LayersInfo.Size + 1)
+                            LayersInfo.Size += 1
+                            LayersInfo.Arr(LayersInfo.Size).Name = LayerName
+                            LayersInfo.Arr(LayersInfo.Size).Id = LayerId
+
+                        End If
+                    End While
+                End While
+            Catch ex As Exception
+                ' do nothing
+                'MessageBox.Show(ex.ToString())
+            End Try
+        End Using
+    End Sub
+
+    Private Sub DisplayLayersInfo()
+
+        ListBox1.Items.Clear()
+        For Each layer As LayerRec In LayersInfo.Arr
+            ListBox1.Items.Add(layer.Name & "-" & layer.Id)
+        Next
 
     End Sub
 End Class
